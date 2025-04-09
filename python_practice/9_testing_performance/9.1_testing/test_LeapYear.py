e88fbbe1691b4ed7832336e702660957f21949e6
Jeong-su Shin <jeshin@cx2r1s1.42seoul.kr>
2025-04-09 16:47:51 +0900

    add:testing code in 9_testing

--- python_practice/9_testing_performance/9.1_testing/test_LeapYear.py
+++ python_practice/9_testing_performance/9.1_testing/test_LeapYear.py
@@ -3,8 +3,13 @@ import unittest
 
 class TestLeapYear(unittest.TestCase):
     def test_2020(self):
-        r=LeapYear.is_leap_year(2020)
-        self.asserEtEqual(r, True)
+        r = LeapYear.is_leap_year(2020)
+        self.assertEqual(r, True)
+
+class TestLeapYearFalse(unittest.TestCase):
+    def test_1900(self):
+        r = LeapYear.is_leap_year(1900)
+        self.assertEqual(r, False)
 
 if __name__=='__main__':
-    unittest.main
+    unittest.main()
